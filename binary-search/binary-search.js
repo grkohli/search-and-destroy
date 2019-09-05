eee73dfd6979e300a68dd9da1ddf457a1f9299c6
Gundeep Kohli <gkohli1@fordham.edu>
2019-09-05 16:08:55 -0400

    completed binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,34 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let slicedArray = [];
+	let found = false;
+
+	if(array.length === 1) {
+		if(target === array[0]) {
+			found = true;
+		} else {
+			found = false;
+		}
+	} else if(array.length > 1) {
+		let midpoint = Math.floor(array.length/2);
+		if(target < array[midpoint]) {
+			slicedArray = array.slice(0, midpoint);
+			found = binarySearch(slicedArray, target);
+		} else if(target > array[midpoint]){
+			slicedArray = array.slice(midpoint + 1);
+			found = binarySearch(slicedArray, target);
+		} else if(target === array[midpoint]){
+			found = true;
+		}
+	} 
+  return found;
+  
+	// if(array.includes(target)) {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
 };
 
 /*
