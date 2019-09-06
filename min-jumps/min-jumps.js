46ed343588199d1cc73b224b2fda15f62d4ae165
Gundeep Kohli <gkohli1@fordham.edu>
2019-09-06 10:08:17 -0400

    started jumpman

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,24 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
+let counter = 0
 
+const minJumps = arr => {
+    if(arr.length <= 1) {
+        return counter;
+    } if(arr.length === 2) {
+        return ++counter;
+    } else {
+        let currValue = arr[0];
+        for(let i = 0; i < arr.length;) {
+            let tempArr = arr.slice(i + 1, i + currValue);
+            let max = Math.max(tempArr);
+            let jumpIndex = arr.indexOf(max)
+            currValue = arr[jumpIndex];
+            counter++;
+            // i +
+        }
+    }
 };
 
 module.exports = minJumps
